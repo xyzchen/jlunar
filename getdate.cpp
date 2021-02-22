0446560ab891f9acbd36465b8cbfff63b98d7ac7
Yishao Chen <jmchxy@gmail.com>
2021-02-22 13:07:16 +0800

    为命令行工具添加帮助文本

--- getdate.cpp
+++ getdate.cpp
@@ -52,26 +52,42 @@ static void printDateString(const LUNARDATE& lunardate, int style=0)
 	}
 }
 
+//帮助字符串
+void usage(void)
+{
+	printf(
+	"\n获取当前农历日期\n"
+	 "用法: getdate [-s|l|f]\n"
+	 "  -s    短格式：农历xx年xx月xx\n"
+	 "  -l    长格式：xx年xx月xx日[农历xx年xx月xx] 星期x\n"
+	 "  -f    “:”分隔的字段：公历:农历:星期x\n");
+	exit(1);
+}
+
 //test: s - 短格式， l - 长格式， f - 分隔的字段
 int main(int argc, char *argv[])
 {
 	int style  = 1;
 	//解析命令行
 	int result;
-	while((result = getopt(argc, argv, "slf")) != -1)
+	while((result = getopt(argc, argv, "slfh")) != -1)
 	{
 		switch(result)
 		{
-			case 's':	//图形样式
+			case 's':	//短格式
 				style = 0;
 				break;
 			
-			case 'l':	//文字模式
+			case 'l':	//长格式
 				style = 1;
 				break;
 				
-			case 'f':	//最大值
+			case 'f':	//分隔
 				style = 2;
+				break;
+
+			case 'h':	//打印帮助
+				usage();
 				break;
 				
 			default:
